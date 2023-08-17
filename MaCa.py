bddadf404c1652eda8eea985558cdce3b20746f2
habrvalg <kopasov.andrej@gmail.com>
2023-08-18 06:41:03 +1000

    Qt.0.2
    Testing PyQt5

--- MaCa.py
+++ MaCa.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QTextEdit, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QTextEdit, QPushButton, QVBoxLayout, QWidget
 
 
 class MatrixCalculatorApp(QMainWindow):
@@ -12,30 +12,36 @@ class MatrixCalculatorApp(QMainWindow):
         self.init_ui()
 
     def init_ui(self):
-        self.operation_label = QLabel("Выберите операцию:")
-        self.operation_menu = QLineEdit()
-        self.operation_menu.setPlaceholderText("Сложение / Вычитание / Умножение на число / Умножение")
-
-        self.matrix1_label = QLabel("Введите матрицу 1:")
+        self.matrix1_label = QLabel("Введите матрицу 1 (разделяйте числа пробелами, строки - переносами):")
         self.matrix1_entry = QTextEdit()
 
-        self.matrix2_label = QLabel("Введите матрицу 2:")
+        self.matrix2_label = QLabel("Введите матрицу 2 (разделяйте числа пробелами, строки - переносами):")
         self.matrix2_entry = QTextEdit()
-
-        self.calculate_button = QPushButton("Вычислить")
-        self.calculate_button.clicked.connect(self.calculate)
 
         self.result_label = QLabel("Результат:")
         self.result_display = QTextEdit()
 
+        self.addition_button = QPushButton("Сложение")
+        self.addition_button.clicked.connect(self.calculate_addition)
+
+        self.subtraction_button = QPushButton("Вычитание")
+        self.subtraction_button.clicked.connect(self.calculate_subtraction)
+
+        self.scalar_mult_button = QPushButton("Умножение на число")
+        self.scalar_mult_button.clicked.connect(self.calculate_scalar_mult)
+
+        self.matrix_mult_button = QPushButton("Умножение")
+        self.matrix_mult_button.clicked.connect(self.calculate_matrix_mult)
+
         layout = QVBoxLayout()
-        layout.addWidget(self.operation_label)
-        layout.addWidget(self.operation_menu)
         layout.addWidget(self.matrix1_label)
         layout.addWidget(self.matrix1_entry)
         layout.addWidget(self.matrix2_label)
         layout.addWidget(self.matrix2_entry)
-        layout.addWidget(self.calculate_button)
+        layout.addWidget(self.addition_button)
+        layout.addWidget(self.subtraction_button)
+        layout.addWidget(self.scalar_mult_button)
+        layout.addWidget(self.matrix_mult_button)
         layout.addWidget(self.result_label)
         layout.addWidget(self.result_display)
 
@@ -43,23 +49,32 @@ class MatrixCalculatorApp(QMainWindow):
         container.setLayout(layout)
         self.setCentralWidget(container)
 
-    def calculate(self):
-        operation = self.operation_menu.text()
+    def get_matrices(self):
         matrix1 = np.array(
             [[float(entry) for entry in row.split()] for row in self.matrix1_entry.toPlainText().split("\n")])
         matrix2 = np.array(
             [[float(entry) for entry in row.split()] for row in self.matrix2_entry.toPlainText().split("\n")])
+        return matrix1, matrix2
 
-        if operation == "Сложение":
-            result = np.add(matrix1, matrix2)
-        elif operation == "Вычитание":
-            result = np.subtract(matrix1, matrix2)
-        elif operation == "Умножение на число":
-            scalar = float(input("Введите число для умножения: "))
-            result = np.multiply(scalar, matrix1)
-        elif operation == "Умножение":
-            result = np.dot(matrix1, matrix2)
+    def calculate_addition(self):
+        matrix1, matrix2 = self.get_matrices()
+        result = np.add(matrix1, matrix2)
+        self.result_display.setText("Результат:\n" + str(result))
 
+    def calculate_subtraction(self):
+        matrix1, matrix2 = self.get_matrices()
+        result = np.subtract(matrix1, matrix2)
+        self.result_display.setText("Результат:\n" + str(result))
+
+    def calculate_scalar_mult(self):
+        matrix1, _ = self.get_matrices()
+        scalar = float(input("Введите число для умножения: "))
+        result = np.multiply(scalar, matrix1)
+        self.result_display.setText("Результат:\n" + str(result))
+
+    def calculate_matrix_mult(self):
+        matrix1, matrix2 = self.get_matrices()
+        result = np.dot(matrix1, matrix2)
         self.result_display.setText("Результат:\n" + str(result))
 
 
