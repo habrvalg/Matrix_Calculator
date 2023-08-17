8bdedde4fc1af626935f583c302cfe444e6bc4ea
habrvalg <kopasov.andrej@gmail.com>
2023-08-18 06:35:22 +1000

    Tkinter testing

--- MaCa.py
+++ MaCa.py
@@ -1,48 +1,65 @@
 import numpy as np
+import tkinter as tk
+from tkinter import ttk
 
 
-def main():
-    print("Матричный калькулятор")
-    print("1. Сложение матриц")
-    print("2. Вычитание матриц")
-    print("3. Умножение матрицы на число")
-    print("4. Умножение матриц")
-    choice = int(input("Выберите операцию (1/2/3/4): "))
+class MatrixCalculatorApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Матричный калькулятор")
 
-    if choice in [1, 2, 3, 4]:
-        matrix1 = input_matrix("Введите первую матрицу:")
-        if choice != 3:
-            matrix2 = input_matrix("Введите вторую матрицу:")
+        self.create_widgets()
 
-        if choice == 1:
-            result = matrix1 + matrix2
-        elif choice == 2:
-            result = matrix1 - matrix2
-        elif choice == 3:
+    def create_widgets(self):
+        self.operation_var = tk.StringVar()
+        self.operation_var.set("Сложение")
+
+        operation_label = tk.Label(self.root, text="Выберите операцию:")
+        operation_label.pack(pady=10)
+
+        operation_menu = ttk.Combobox(self.root, textvariable=self.operation_var,
+                                      values=["Сложение", "Вычитание", "Умножение на число", "Умножение"])
+        operation_menu.pack()
+
+        matrix1_label = tk.Label(self.root, text="Введите матрицу 1:")
+        matrix1_label.pack(pady=10)
+
+        self.matrix1_entry = tk.Text(self.root, height=5, width=20)
+        self.matrix1_entry.pack()
+
+        matrix2_label = tk.Label(self.root, text="Введите матрицу 2:")
+        matrix2_label.pack(pady=10)
+
+        self.matrix2_entry = tk.Text(self.root, height=5, width=20)
+        self.matrix2_entry.pack()
+
+        calculate_button = tk.Button(self.root, text="Вычислить", command=self.calculate)
+        calculate_button.pack(pady=10)
+
+        self.result_label = tk.Label(self.root, text="Результат:")
+        self.result_label.pack()
+
+    def calculate(self):
+        operation = self.operation_var.get()
+        matrix1 = np.array([[float(entry) for entry in row.split()] for row in
+                            self.matrix1_entry.get("1.0", "end").strip().split("\n")])
+        matrix2 = np.array([[float(entry) for entry in row.split()] for row in
+                            self.matrix2_entry.get("1.0", "end").strip().split("\n")])
+
+        if operation == "Сложение":
+            result = np.add(matrix1, matrix2)
+        elif operation == "Вычитание":
+            result = np.subtract(matrix1, matrix2)
+        elif operation == "Умножение на число":
             scalar = float(input("Введите число для умножения: "))
-            result = scalar * matrix1
-        elif choice == 4:
+            result = np.multiply(scalar, matrix1)
+        elif operation == "Умножение":
             result = np.dot(matrix1, matrix2)
 
-        print("Результат:")
-        print(result)
-    else:
-        print("Некорректный выбор операции.")
-
-
-def input_matrix(prompt):
-    rows = int(input(prompt + " Количество строк: "))
-    cols = int(input(prompt + " Количество столбцов: "))
-    matrix = []
-    print("Введите элементы матрицы:")
-    for i in range(rows):
-        row = []
-        for j in range(cols):
-            element = float(input(f"Элемент [{i}, {j}]: "))
-            row.append(element)
-        matrix.append(row)
-    return np.array(matrix)
+        self.result_label.config(text="Результат:\n" + str(result))
 
 
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    app = MatrixCalculatorApp(root)
+    root.mainloop()
